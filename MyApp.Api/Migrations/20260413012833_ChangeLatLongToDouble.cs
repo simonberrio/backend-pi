5ffb6a24bdc060c7f8e5aead2e6c43fe748a86f1
SimonBerrio <logistica@altos-soft.com>
2026-04-12 20:29:20 -0500

    Migración para cambiar la latitud y la longitud a double

---
+++ MyApp.Api/Migrations/20260413012833_ChangeLatLongToDouble.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MyApp.Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class ChangeLatLongToDouble : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<double>(
+                name: "Longitude",
+                table: "Events",
+                type: "double precision",
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "numeric");
+
+            migrationBuilder.AlterColumn<double>(
+                name: "Latitude",
+                table: "Events",
+                type: "double precision",
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "numeric");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "UpdatedDate",
+                table: "Events",
+                type: "timestamp with time zone",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "UpdatedDate",
+                table: "Events");
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "Longitude",
+                table: "Events",
+                type: "numeric",
+                nullable: false,
+                oldClrType: typeof(double),
+                oldType: "double precision");
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "Latitude",
+                table: "Events",
+                type: "numeric",
+                nullable: false,
+                oldClrType: typeof(double),
+                oldType: "double precision");
+        }
+    }
+}
